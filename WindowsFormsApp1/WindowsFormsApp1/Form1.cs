e62086a8200de307c19a278cc976e49ce628d365
roger90810 <43696670+roger90810@users.noreply.github.com>
2019-03-25 02:38:13 +0800

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using Npgsql;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public string aesEncrypt(string sourceStr, string cryptKey, string cryptIV)
+        {
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();              // MD5      Hash計算
+            SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();     // SHA256   Hash計算
+
+            byte[] key = md5.ComputeHash(Encoding.UTF8.GetBytes(cryptKey));             // 計算AES的key
+            byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(cryptIV));               // 計算AES的IV
+            byte[] dataByteArray = Encoding.UTF8.GetBytes(sourceStr);                   // 將輸入字串轉為byte
+            aes.Key = key;
+            aes.IV = iv;
+            ICryptoTransform cryptoTransform = aes.CreateEncryptor(aes.Key, aes.IV);    // encrypt
+            byte[] outputData = cryptoTransform.TransformFinalBlock(dataByteArray, 0, dataByteArray.Length);    //將encrypt後的array存到output
+            return BitConverter.ToString(outputData).Replace("-", "");  //轉成hex string return
+
+        }
+        public string aesDecrypt(string sourceHexStr, string cryptKey, string cryptIV)
+        {
+
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();              // MD5      Hash計算
+            SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();     // SHA256   Hash計算
+
+            byte[] key = md5.ComputeHash(Encoding.UTF8.GetBytes(cryptKey));             // 計算AES的key
+            byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(cryptIV));               // 計算AES的IV
+
+            byte[] dataByteArray = Enumerable.Range(0, sourceHexStr.Length)
+                                        .Where(x => x % 2 == 0)
+                                        .Select(x => Convert.ToByte(sourceHexStr.Substring(x, 2), 16))
+                                        .ToArray();                // 將輸入的Hex字串轉為byte
+            aes.Key = key;
+            aes.IV = iv;
+            ICryptoTransform cryptoTransform = aes.CreateDecryptor(aes.Key, aes.IV);    // decrypt
+            byte[] outputData = cryptoTransform.TransformFinalBlock(dataByteArray, 0, dataByteArray.Length);    //將decrypt後的array存到output
+            return Encoding.UTF8.GetString(outputData);  //轉成UTF的string return
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string encrypt = aesEncrypt("abc123", "aaaaaaaaaab", "aab");
+            string decrypt = aesDecrypt(encrypt, "aaaaaaaaaab", "aab");
+            textBox1.AppendText(encrypt + "\r\n");
+            textBox1.AppendText(decrypt + "\r\n");
+
+            /*string base64Decoded = aesEncrypt("abc123", "aaaaaaaaaab");
+            string base64Encoded;
+            byte[] data = System.Text.ASCIIEncoding.ASCII.GetBytes(base64Decoded);
+            base64Encoded = System.Convert.ToBase64String(data);
+
+            textBox1.AppendText(base64Encoded + "\r\n");*/
+
+            /*PostgreSQL postgreSQL = new PostgreSQL();
+            List<string> rst = postgreSQL.PostgreSQLtest();
+            foreach (string s in rst)
+            {
+                Console.WriteLine(s);
+                textBox1.AppendText(s);
+            }*/
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/PostgreSQL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace WindowsFormsApp1
+{
+    public class PostgreSQL
+    {
+        List<string> dataItems = new List<string>();
+        public PostgreSQL()
+        {
+
+        }
+        public List<string> PostgreSQLtest()
+        {
+            try
+            {
+                string connStr = "Server=127.0.0.1; Port=5432; User Id=postgres; Password=0000; Database=postgres;";
+                NpgsqlConnection npgsqlconnection = new NpgsqlConnection(connStr);
+                npgsqlconnection.Open();
+                NpgsqlCommand npgsqlCommand = new NpgsqlCommand("SELECT * FROM public.\"Student\"", npgsqlconnection);
+                NpgsqlDataReader npgsqlDataReader = npgsqlCommand.ExecuteReader();
+                while (npgsqlDataReader.Read()){
+                    dataItems.Add(npgsqlDataReader[0].ToString() + "," + npgsqlDataReader[1].ToString() + ","
+                        + npgsqlDataReader[2].ToString() + "," + npgsqlDataReader[3].ToString() + "\r\n");
+                }
+                Console.WriteLine("Done");
+                npgsqlconnection.Close();
+                return dataItems;
+            }
+            catch(Exception msg)
+            {
+                Console.WriteLine(msg);
+                throw;
+            }
+        }
+    }
+}
